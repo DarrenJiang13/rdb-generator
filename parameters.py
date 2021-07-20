352dcc73db9ec48b5b33d153d06834e110922e4b
DarrenJiang13 <yjjiang1996@163.com>
2021-07-21 00:13:26 +0800

    Create parameters.py

---
+++ parameters.py
@@ -0,0 +1,30 @@
+# string-int
+string_int_num = 512
+# string-embstr
+string_emb_num = 512
+# string-raw
+string_raw_num = 512
+string_raw_max_field_len = 200
+# list-quicklist
+list_num = 512
+list_max_field_len = 200
+# set-intset
+set_intset_num = 512
+# set-dict
+set_dict_num = 512
+set_dict_max_field_len = 300
+# hash-dict
+hash_dict_num = 1024
+hash_dict_max_field_len = 300
+# hash-ziplist
+hash_ziplist_num = 128
+hash_ziplist_max_field_len = 63
+# zset-ziplist
+zset_ziplist_num = 128
+zset_ziplist_max_field_len = 63
+# zset-skiplist
+zset_skiplist_num = 1024
+zset_skiplist_max_field_len = 200
+# stream-stream
+stream_message_num = 1024
+stream_max_field_len = 250
